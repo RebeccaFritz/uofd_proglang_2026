817c327e2968c913558edaf5caff965e8160c94a
Rebecca Fritz <rgfritz13@gmail.com>
2026-03-05 13:46:17 -0600

    homework 10

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -2,6 +2,7 @@
 enum Primitive {
     Add,
     Multiply,
+    Subtract,
     Number(i32)
 }
 
@@ -18,13 +19,23 @@ fn evaluate(primitives: Vec<Primitive>) -> i32 {
             eval_prim(&primitives[2]) },
         Primitive::Multiply => { eval_prim(&primitives[1]) *
             eval_prim(&primitives[2]) },
+        Primitive::Subtract => { eval_prim(&primitives[1]) - 
+            eval_prim(&primitives[2]) },
         Primitive::Number(_) => 0
     } 
 }
 
 fn main() {
 
-    let result: bool = true;
-    println!("{result}")
+    // this is using Polish Notation (Prefix notiation) so (- 3 4) is equivalent to (3 - 4)
+    let mut primitives = Vec::<Primitive>::new();
+    primitives.push(Primitive::Subtract); 
+    primitives.push(Primitive::Number(3));
+    primitives.push(Primitive::Number(4));
+    let result = evaluate(primitives);
+    println!("The result is {result}");
 
 }
+
+
+
