7b105a4624728f154a47b6627087565568475926
Rebecca Fritz <rgfritz13@gmail.com>
2026-04-19 14:03:20 -0500

    homework 17

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,3 +1,7 @@
+// Rebecca Fritz
+#[derive(Debug)]
+#[derive(PartialEq)]
+
 enum Expression {
     Integer(i32),
     FixedPoint(i32, i32),
@@ -16,11 +20,31 @@ fn evaluate_add_integers(expressions: &Vec<Expression>) -> Expression {
     Expression::Integer(total)
 } 
 
+fn evaluate_add_fixed_point(expressions: &Vec<Expression>) -> Expression {
+    let mut total_whole = 0;
+    let mut total_frac = 0;
+    for each in expressions {
+        if let Expression::FixedPoint(whole, frac) = each {
+            total_whole = total_whole + whole;
+            total_frac = total_frac + frac;
+            if total_frac > 99 {
+                total_whole = total_whole + 1;
+                total_frac = total_frac - 100;
+            }
+        } else {
+            panic!("non-fixed-point value provided in vector");
+        }
+    }
+    
+    Expression::FixedPoint(total_whole, total_frac)
+}
+
 fn evaluate_addition(expression: &Expression) -> Expression {
     if let Expression::Addition(expressions) = expression {
         match expressions[0] {
             Expression::Integer(_) => evaluate_add_integers(expressions),
-            _ => panic!("I only know integers at the moment...")
+            Expression::FixedPoint(_, _) => evaluate_add_fixed_point(expressions),
+            _ => panic!("Addition is only provided for integers and fixed-points")
         }
     } else {
         panic!("not addition");
@@ -73,6 +97,58 @@ mod tests {
         let val = crate::evaluate(&expr);
         // assert
         assert_eq!(val, 4.0, "expressions not equal");
+    }
+
+    #[test]
+    fn test_evaluate_fixed_point() {
+        // arrange
+        let expr = crate::Expression::FixedPoint(2,5);
+        // act
+        let val = crate::evaluate_fixed_point(&expr);
+        // assert
+        assert_eq!(val, 2.05, "expressions not equal");
+    }
+
+    #[test]
+    fn test_fixed_point_addition() {
+        // arrange
+        let expr = crate::Expression::Addition(vec![
+            crate::Expression::FixedPoint(2,3),
+            crate::Expression::FixedPoint(4,25)
+        ]);
+        let expected = crate::Expression::FixedPoint(6, 28);
+        // act
+        let val = crate::evaluate_addition(&expr);
+        // assert
+        assert_eq!(val, expected, "expressions not equal");
+    }
+
+    #[test]
+    fn test_fixed_point_addition_direct() {
+        // arrange
+        let expr = vec![
+            crate::Expression::FixedPoint(4,11),
+            crate::Expression::FixedPoint(5,13)
+        ];
+        let expected = crate::Expression::FixedPoint(9, 24);
+        // act
+        let val = crate::evaluate_add_fixed_point(&expr);
+        // assert
+        assert_eq!(val, expected, "expressions not equal");
+    }
+
+    #[test]
+    fn test_fixed_point_addition_big_decimals() {
+        // arrange
+        let expr = vec![
+            crate::Expression::FixedPoint(4,92),
+            crate::Expression::FixedPoint(5,56)
+        ];
+        let expected = crate::Expression::FixedPoint(10, 48);
+        // act
+        let val = crate::evaluate_add_fixed_point(&expr);
+        // assert
+        assert_eq!(val, expected, "expressions not equal");
     }
 }
 
